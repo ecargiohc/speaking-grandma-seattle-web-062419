6c531cde4a0979f390403a6dde688e1ca07e9b39
Grace Colette <ilovegrace09@gmail.com>
2019-06-18 20:17:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ phrase = "I LOVE YOU GRANDMA!"
 def speak_to_grandma(phrase)
   if phrase != "#{phrase}".upcase 
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "#{phrase}".upcase 
+  elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else  
     return "NO, NOT SINCE 1938!"
